3a8eb820cdcb098d2d6c25614e6193a1e4c5a241
André Rakvåg <andre.rakvag@nav.no>
2020-09-13 13:32:30 +0200

    Endre til å vise info for bare en konto

--- src/main/kotlin/org/rakvag/finans/RootController.kt
+++ src/main/kotlin/org/rakvag/finans/RootController.kt
@@ -1,5 +1,6 @@
 package org.rakvag.finans
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,13 +8,23 @@ import java.security.Principal
 
 @RestController("/")
 class RootController(
+        @Value("\${KONTONUMMER}") private val kontonummer: String,
         private val sbankenClient: SbankenClient
 ) {
 
     @GetMapping("")
     fun helloWorld(principal: Principal): ResponseEntity<String> {
-        val response = sbankenClient.getAccountInfo()
-        return ResponseEntity.ok("Hello world.<br>Response: $response")
+        val kontoinformasjon = sbankenClient.getAccountInfo()
+        val brukskonto = kontoinformasjon.items.find { it.accountNumber == kontonummer }
+                ?: throw RuntimeException("Fant ikke kontoen")
+        val formatertKonto = formaterKonto(brukskonto)
+        return ResponseEntity.ok("Konto $formatertKonto har disponibelt ${brukskonto.available} kr.")
+    }
+
+    private fun formaterKonto(konto: AccountInfo): String {
+        val kontonummer = "${kontonummer.subSequence(0, 4)} ${kontonummer.subSequence(4, 6)} ${kontonummer.subSequence(6, 11)}"
+
+        return "$kontonummer (${konto.name})"
     }
 
 }
